fe5e0c19b4888044af26887c9af45a2b270b2fbc
Avik Jain <avikjain12345@gmail.com>
2017-07-02 23:49:31 -0700

    add stats display functionality with matplotlib histogram

--- console_2048.py
+++ console_2048.py
@@ -1,6 +1,8 @@
 import sys
 import argparse
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 from game import GameBoard
 
 
@@ -11,6 +13,7 @@ def main(args):
       's': GameBoard.MOVE_DOWN,
       'd': GameBoard.MOVE_RIGHT,
   }
+  scores = []
   for n in range(args.nrof_games):
     if args.nrof_games != 1:
       if not args.quiet:
@@ -43,12 +46,23 @@ def main(args):
       print board
       print 'Game Over.'
     print 'Score: %d' % board.current_score()
-
+    scores.append(board.current_score())
+  if args.show_stats:
+    print '\n\n\nScore Stats'
+    print '==========='
+    print 'Mean score: %.2f' % np.mean(scores)
+    print 'High score: %d' % np.amax(scores)
+    print 'Low score: %d' % np.amin(scores)
+    print 'Standard deviation: %.2f' % np.std(scores)
+    plt.hist(scores, bins='auto')
+    plt.title('2048 Score Distribution')
+    plt.show()
 
 def parse_arguments(argv):
   parser = argparse.ArgumentParser()
   parser.add_argument('-r', '--random_moves', help='Switch to run random simulation', action='store_true')
   parser.add_argument('-n', '--nrof_games', help='Number of games to play.', type=int, default=1)
   parser.add_argument('-q', '--quiet', help='Switch to prevent printing boards.', action='store_true')
+  parser.add_argument('-s', '--show_stats', help='Switch to show score stats.', action='store_true')
   return parser.parse_args(argv)
 main(parse_arguments(sys.argv[1:]))
